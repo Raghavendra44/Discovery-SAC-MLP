8a3e896463d5072d97851f5020a69df7727c6ccd
Raghavendra44 <67100596+Raghavendra44@users.noreply.github.com>
2025-06-10 19:47:59 +0530

    Add files via upload

--- create_slabs.py
+++ create_slabs.py
@@ -7,9 +7,9 @@ doping_sites = {"211": [10, 2, 22], "111": [58], "110": [58], "100": [58]}
 dopants = [13, 31, 32, 49, 50, 51, 81, 82, 83] + list(set(range(21, 31)) - {29}) + list(set(range(39, 49)) - {43}) + list(range(71, 80))
 
 for mi in slab_func_map:
-    slab = slab_func_map[mi]("Cu", size = size_map[mi], orthogonal = True, vacuum = 10)
     for site in doping_sites[mi]:
         for dopant in dopants:
+            slab = slab_func_map[mi]("Cu", size = size_map[mi], orthogonal = True, vacuum = 10)
             slab.numbers[site] = dopant
             filename = f"Cu{mi}_{dopant}_{site}.POSCAR"
             write(filename, slab)
