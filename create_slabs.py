cfe48a6b22886373d360f490645d9b34ed26e662
Raghavendra44 <67100596+Raghavendra44@users.noreply.github.com>
2025-06-17 23:51:14 +0530

    Update create_slabs.py
    Removed path info

--- create_slabs.py
+++ create_slabs.py
@@ -8,8 +8,8 @@ doping_sites = {"211": [10, 2, 22], "111": [58], "110": [58], "100": [58]}
 dopants = [13, 31, 32, 49, 50, 51, 81, 82, 83] + list(set(range(21, 31)) - {29}) + list(set(range(39, 49)) - {43}) + list(range(72, 80))
 print(f"Number of dopants = {len(dopants)}")
 print(f"Total number of surfaces = {6*len(dopants)}")
-path = "D:/ACADEMIC DOCUMENTS/IISc/CO2_act/RELAXED SUPERCELLS - OC20/"
-save_path = "D:/ACADEMIC DOCUMENTS/IISc/CO2_act/DOPED_RELAXED/"
+path = "<PATH>"
+save_path = "<PATH>"
 for mi in slab_func_map:
     for site in doping_sites[mi]:
         for dopant in dopants:
